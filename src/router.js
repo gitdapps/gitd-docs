5265bcef35de7d89639c71da048b970836f2aa80
Isaiah Simpson <53048243+isaiahsimpson@users.noreply.github.com>
2022-04-17 15:43:31 -0500

    hash history :(

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import DocView from "@/components/doc-view.vue";
 import HomeView from "@/components/home-view.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
